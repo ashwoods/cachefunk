64ee2b001e6e214bf3b958b3931cfbefd74fe4dc
Ashley Camba Garrido <ashwoods@gmail.com>
2017-09-22 07:39:45 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*parts):
 
 setup(
     name="cachefunk",
-    version="0.1.0",
+    version="0.1.1",
     url='https://github.com/ashwoods/cachefunk',
     license='MIT',
     description="Sitemap.xml cache warmer",
